256eb705a7f0f79ad14d9e82bbe234853ac9d386
Arminder Singh <E231201@corp.corpcommon.com>
2019-01-07 11:53:16 +0530

    Added TDD project

---
+++ BlogService.TDD/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlogService.TDD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
