60cacc1d362b167b847757ace805029adc91be3d
Arminder Singh <E231201@corp.corpcommon.com>
2019-01-07 18:38:26 +0530

    check 3

--- BlogService.TDD/UnitTest1.cs
+++ BlogService.TDD/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace BlogService.TDD
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(1,1);
+            Assert.AreEqual(1,2);
         }
     }
 }
