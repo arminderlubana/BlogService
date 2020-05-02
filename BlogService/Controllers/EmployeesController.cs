981b46122cfe2bf61eddb8138b666f8d28d93481
arminderlubana <arminder.lubana@gmail.com>
2020-05-02 15:24:10 +0530

    Update EmployeesController.cs

--- BlogService/Controllers/EmployeesController.cs
+++ BlogService/Controllers/EmployeesController.cs
@@ -1,4 +1,4 @@
-﻿using BlogService.Data;
+using BlogService.Data;
 using BlogService.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
@@ -41,6 +41,8 @@ namespace BlogService.Controllers
             }
 
             var employee = await _context.Employees.SingleOrDefaultAsync(m => m.EmployeeId == id);
+            employee.Skills = _context.Skills.Where(m => m.EmployeeId == id).ToList();
+            employee.Experiences = _context.Experiences.Where(m => m.EmployeeId == id).ToList();
 
             if (employee == null)
             {
@@ -67,10 +69,30 @@ namespace BlogService.Controllers
             }
 
             _context.Entry(employee).State = EntityState.Modified;
+            _context.Skills.RemoveRange(_context.Skills.Where(s => s.EmployeeId == employee.EmployeeId));
+            _context.Experiences.RemoveRange(_context.Experiences.Where(s => s.EmployeeId == employee.EmployeeId));
+            //var skills = new List<Skills>();
+            //var experience = new List<Experience>();
+
+
+
 
             try
             {
                 await _context.SaveChangesAsync();
+                if (employee.Skills != null && employee.Skills.Any())
+                {
+                    employee.Skills.ToList().ForEach(s => s.Id = 0);
+                    _context.Skills.AddRange(employee.Skills);
+                    await _context.SaveChangesAsync();
+                }
+                if (employee.Experiences != null && employee.Experiences.Any())
+                {
+                    employee.Experiences.ToList().ForEach(s => s.Id = 0);
+                    _context.Experiences.AddRange(employee.Experiences);
+                    await _context.SaveChangesAsync();
+                    
+                }
                 emp = await _context.Employees.SingleOrDefaultAsync(m => m.EmployeeId == id);
             }
             catch (DbUpdateConcurrencyException)
@@ -96,15 +118,26 @@ namespace BlogService.Controllers
             {
                 return BadRequest(ModelState);
             }
-
+            var skills= new List<Skills>();
+            var experience= new List<Experience>();
+            if(employee.Skills!=null)
+                 skills = employee.Skills.ToList();
+            if(employee.Experiences != null)
+             experience = employee.Experiences.ToList();
+            employee.Skills = null;
+            employee.Experiences = null;
             _context.Employees.Add(employee);
             try
             {
                 await _context.SaveChangesAsync();
-
+                skills.ForEach(s => s.EmployeeId = employee.EmployeeId);
+                _context.Skills.AddRange(skills);
+                experience.ForEach(e => e.EmployeeId = employee.EmployeeId);
+                _context.Experiences.AddRange(experience);
+                await _context.SaveChangesAsync();
                 return CreatedAtAction("GetEmployee", new { id = employee.EmployeeId }, employee);
             }
-            catch (System.Exception)
+            catch (System.Exception ex)
             {
 
                 throw;
